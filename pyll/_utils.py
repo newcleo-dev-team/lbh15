0c457880aec5f4295beb5063fa7f870d675f44d9
newcleo <newcleo@ubuntu.com>
2022-07-15 11:26:40 +0200

    [master] added class that takes properties as input

--- pyll/_utils.py
+++ pyll/_utils.py
@@ -1,4 +1,34 @@
+import math
 from ._pyll import MELTING_TEMPERATURE
+
+
+def p_s(T):
+    return 1.88e13 * T * math.exp(-23325/T)
+
+
+def sigma(T):
+    return (525.9 - 0.113*T)/1000
+
+
+def rho(T):
+    return 11441 - 1.2795*T
+
+
+def alpha(T):
+    return 1/(8942 - T)
+
+
+def u_s(T):
+    return 1953 - 0.246*T
+
+
+def beta_s(T):
+    return 1/(rho(T) * u_s(T)**2)
+
+
+def cp(T):
+    return (175.1 - 4.961e-2*T + 1.985e-5*T**2
+            - 2.099e-9*T**3 - 1.524e6*T**-2)
 
 
 def delta_h(T):
@@ -6,3 +36,7 @@ def delta_h(T):
             - 2.4615e-2*(T**2 - MELTING_TEMPERATURE**2)
             + 5.147e-6*(T**3 - MELTING_TEMPERATURE**3)
             + 1.524e6*(T**-1 - MELTING_TEMPERATURE**-1))
+
+
+def mi(T):
+    return 4.55e-4*math.exp(1069/T)
--- pyll/properties.py
+++ pyll/properties.py
@@ -1,11 +1,11 @@
-import math
 from scipy.optimize import fsolve
-import numpy as np
+import math
 from ._pyll import ZERO_C_IN_K, CELSIUS_SYMBOL
 from ._pyll import KELVIN_SYMBOL, MELTING_TEMPERATURE
 from ._pyll import MELTING_LATENT_HEAT, BOILING_TEMPERATURE
 from ._pyll import VAPORISATION_HEAT
-from ._utils import delta_h
+from ._utils import p_s, delta_h, sigma, rho, alpha, u_s
+from ._utils import beta_s, cp, mi
 
 
 def _get_temperature_in_kelvin(temperature, units):
@@ -51,16 +51,15 @@ class LeadProperties(object):
             self.__Q_m0 = MELTING_LATENT_HEAT
             self.__T_b0 = BOILING_TEMPERATURE
             self.__Q_b0 = VAPORISATION_HEAT
-            self.__p_s = 1.88e13 * self.T * math.exp(-23325/self.T)
-            self.__sigma = (525.9 - 0.113*self.T)/1000
-            self.__rho = 11441 - 1.2795*self.T
-            self.__alpha = 1/(8942 - self.T)
-            self.__u_s = 1953 - 0.246*self.T
-            self.__beta_s = 1/(self.rho * self.u_s**2)
-            self.__cp = (175.1 - 4.961e-2*self.T + 1.985e-5*self.T**2
-                         - 2.099e-9*self.T**3 - 1.524e6*self.T**-2)
+            self.__p_s = p_s(self.T)
+            self.__sigma = sigma(self.T)
+            self.__rho = rho(self.T)
+            self.__alpha = alpha(self.T)
+            self.__u_s = u_s(self.T)
+            self.__beta_s = beta_s(self.T)
+            self.__cp = cp(self.T)
             self.__delta_h = delta_h(self.T)
-            self.__mi = 4.55e-4*math.exp(1069/self.T)
+            self.__mi = mi(self.T)
 
     def __assign_T(self, T, temperatureUnits):
         temp = _get_temperature_in_kelvin(T, temperatureUnits)
@@ -75,6 +74,10 @@ class LeadProperties(object):
     @property
     def T(self):
         return self.__T
+
+    @property
+    def T_in_celsius(self):
+        return self.T - ZERO_C_IN_K
 
     @property
     def T_assigned(self):
@@ -121,6 +124,10 @@ class LeadProperties(object):
         return self.__beta_s
 
     @property
+    def cp(self):
+        return self.__cp
+
+    @property
     def delta_h(self):
         return self.__delta_h
 
@@ -129,25 +136,57 @@ class LeadProperties(object):
         return self.__mi
 
 
-class LeadPropertiesSigma(LeadProperties):
-    def __init__(self, sigma):
-        temp = 1/0.113*(525.9 - 1000*sigma)
+class _LeadPropertiesFromX(LeadProperties):
+    def __init__(self, function_of_T, target, guess=MELTING_TEMPERATURE*1.5):
+
+        def function_to_solve(T, target):
+            return function_of_T(T) - target
+
+        temp = fsolve(function_to_solve, x0=[guess], args=[target])[0]
         super().__init__(temp, KELVIN_SYMBOL)
 
 
-class LeadPropertiesRho(LeadProperties):
-    def __init__(self, rho):
-        temp = (11441 - rho)/1.2795
-        super().__init__(temp, KELVIN_SYMBOL)
+class LeadPropertiesP_s(_LeadPropertiesFromX):
+    def __init__(self, saturation_pressure, guess=MELTING_TEMPERATURE*1.5):
+        super().__init__(p_s, saturation_pressure, guess)
 
 
-class LeadPropertiesAlpha(LeadProperties):
-    def __init__(self, alpha):
-        temp = 8942 - 1/alpha
-        super().__init__(temp, KELVIN_SYMBOL)
+class LeadPropertiesSigma(_LeadPropertiesFromX):
+    def __init__(self, surface_tension, guess=MELTING_TEMPERATURE*1.5):
+        super().__init__(sigma, surface_tension, guess)
 
 
-class LeadPropertiesU_s(LeadProperties):
-    def __init__(self, u_s):
-        temp = (1953 - u_s)/0.246
-        super().__init__(temp, KELVIN_SYMBOL)
+class LeadPropertiesRho(_LeadPropertiesFromX):
+    def __init__(self, density, guess=MELTING_TEMPERATURE*1.5):
+        super().__init__(rho, density, guess)
+
+
+class LeadPropertiesAlpha(_LeadPropertiesFromX):
+    def __init__(self, expansion_coefficient, guess=MELTING_TEMPERATURE*1.5):
+        super().__init__(alpha, expansion_coefficient, guess)
+
+
+class LeadPropertiesU_s(_LeadPropertiesFromX):
+    def __init__(self, sound_velocity, guess=MELTING_TEMPERATURE*1.5):
+        super().__init__(u_s, sound_velocity, guess)
+
+
+class LeadPropertiesBeta_s(_LeadPropertiesFromX):
+    def __init__(self, adiabatic_compressibility,
+                 guess=MELTING_TEMPERATURE*1.5):
+        super().__init__(beta_s, adiabatic_compressibility, guess)
+
+
+class LeadPropertiesCp(_LeadPropertiesFromX):
+    def __init__(self, specific_heat, guess=MELTING_TEMPERATURE*1.5):
+        super().__init__(cp, specific_heat, guess)
+
+
+class LeadPropertiesDelta_h(_LeadPropertiesFromX):
+    def __init__(self, enthalpy, guess=MELTING_TEMPERATURE*1.5):
+        super().__init__(delta_h, enthalpy, guess)
+
+
+class LeadPropertiesMi(_LeadPropertiesFromX):
+    def __init__(self, dynamic_viscosity, guess=MELTING_TEMPERATURE*1.5):
+        super().__init__(mi, dynamic_viscosity, guess)
