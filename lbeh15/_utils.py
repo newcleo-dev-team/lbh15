e171d988fdd13dd5797794191a0135e886e403e3
newcleo <newcleo@ubuntu.com>
2022-07-26 17:35:48 +0200

    [master] avoided multiple returns

--- lbeh15/_utils.py
+++ lbeh15/_utils.py
@@ -21,7 +21,6 @@ def p_s_initializer(p_s):
     rvalue : float
         Temperature guess in [K]
     """
-    rvalue = 0
     if p_s < 1e-2:
         rvalue = 800
     elif p_s >= 1e-2 and p_s < 1e2:
@@ -45,16 +44,18 @@ def p_s(T, fluid):
         can be one among lead, bismuth or lbe
     """
     if fluid == LEAD_KEYWORD:
-        return 5.76e9 * np.exp(-22131/T)
+        rvalue = 5.76e9 * np.exp(-22131/T)
     elif fluid == BISMUTH_KEYWORD:
-        return 2.67e10 * np.exp(-22858/T)
+        rvalue = 2.67e10 * np.exp(-22858/T)
     elif fluid == LBE_KEYWORD:
-        return 1.22e10 * np.exp(-22552/T)
+        rvalue = 1.22e10 * np.exp(-22552/T)
     else:
         raise ValueError("Fluid can be one among: {:s},"
                          "{:s}, {:s}. {:s} was provided"
                          .format(LEAD_KEYWORD, BISMUTH_KEYWORD,
                                  LBE_KEYWORD, fluid))
+
+    return rvalue
 
 
 def sigma(T, fluid):
@@ -70,16 +71,18 @@ def sigma(T, fluid):
         can be one among lead, bismuth or lbe
     """
     if fluid == LEAD_KEYWORD:
-        return (525.9 - 0.113*T)*1e-3
+        rvalue = (525.9 - 0.113*T)*1e-3
     elif fluid == BISMUTH_KEYWORD:
-        return (420.8 - 0.81*T)*1e-3
+        rvalue = (420.8 - 0.81*T)*1e-3
     elif fluid == LBE_KEYWORD:
-        return (448.5 - 0.0799*T)*1e-3
+        rvalue = (448.5 - 0.0799*T)*1e-3
     else:
         raise ValueError("Fluid can be one among: {:s},"
                          "{:s}, {:s}. {:s} was provided"
                          .format(LEAD_KEYWORD, BISMUTH_KEYWORD,
                                  LBE_KEYWORD, fluid))
+
+    return rvalue
 
 
 def rho(T, fluid):
@@ -95,16 +98,18 @@ def rho(T, fluid):
         can be one among lead, bismuth or lbe
     """
     if fluid == LEAD_KEYWORD:
-        return 11441 - 1.2795*T
+        rvalue = 11441 - 1.2795*T
     elif fluid == BISMUTH_KEYWORD:
-        return 10725 - 1.22*T
+        rvalue = 10725 - 1.22*T
     elif fluid == LBE_KEYWORD:
-        return 11065 - 1.293*T
+        rvalue = 11065 - 1.293*T
     else:
         raise ValueError("Fluid can be one among: {:s},"
                          "{:s}, {:s}. {:s} was provided"
                          .format(LEAD_KEYWORD, BISMUTH_KEYWORD,
                                  LBE_KEYWORD, fluid))
+
+    return rvalue
 
 
 def alpha(T, fluid):
@@ -120,16 +125,18 @@ def alpha(T, fluid):
         can be one among lead, bismuth or lbe
     """
     if fluid == LEAD_KEYWORD:
-        return 1/(8942 - T)
+        rvalue = 1/(8942 - T)
     elif fluid == BISMUTH_KEYWORD:
-        return 1/(8791 - T)
+        rvalue = 1/(8791 - T)
     elif fluid == LBE_KEYWORD:
-        return 1/(8558 - T)
+        rvalue = 1/(8558 - T)
     else:
         raise ValueError("Fluid can be one among: {:s},"
                          "{:s}, {:s}. {:s} was provided"
                          .format(LEAD_KEYWORD, BISMUTH_KEYWORD,
                                  LBE_KEYWORD, fluid))
+
+    return rvalue
 
 
 def u_s(T, fluid):
@@ -145,16 +152,18 @@ def u_s(T, fluid):
         can be one among lead, bismuth or lbe
     """
     if fluid == LEAD_KEYWORD:
-        return 1953 - 0.246*T
+        rvalue = 1953 - 0.246*T
     elif fluid == BISMUTH_KEYWORD:
-        return 1616 + 0.187*T - 2.2e-4*T
+        rvalue = 1616 + 0.187*T - 2.2e-4*T**2
     elif fluid == LBE_KEYWORD:
-        return 1855 - 0.212*T
+        rvalue = 1855 - 0.212*T
     else:
         raise ValueError("Fluid can be one among: {:s},"
                          "{:s}, {:s}. {:s} was provided"
                          .format(LEAD_KEYWORD, BISMUTH_KEYWORD,
                                  LBE_KEYWORD, fluid))
+
+    return rvalue
 
 
 def beta_s(T, fluid):
@@ -185,18 +194,20 @@ def cp(T, fluid):
         can be one among lead, bismuth or lbe
     """
     if fluid == LEAD_KEYWORD:
-        return (175.1 - 4.961e-2*T + 1.985e-5*T**2
-                - 2.099e-9*T**3 - 1.524e6*T**-2)
+        rvalue = (175.1 - 4.961e-2*T + 1.985e-5*T**2
+                  - 2.099e-9*T**3 - 1.524e6*T**-2)
     elif fluid == BISMUTH_KEYWORD:
-        return 118.2 + 5.934e-3*T + 7.183e6*T**-2
+        rvalue = 118.2 + 5.934e-3*T + 7.183e6*T**-2
     elif fluid == LBE_KEYWORD:
-        return (164.8 - 3.94e-2*T + 1.25e-5*T**2
-                - 4.56e5*T**-2)
+        rvalue = (164.8 - 3.94e-2*T + 1.25e-5*T**2
+                  - 4.56e5*T**-2)
     else:
         raise ValueError("Fluid can be one among: {:s},"
                          "{:s}, {:s}. {:s} was provided"
                          .format(LEAD_KEYWORD, BISMUTH_KEYWORD,
                                  LBE_KEYWORD, fluid))
+
+    return rvalue
 
 
 def delta_h(T, fluid):
@@ -212,24 +223,26 @@ def delta_h(T, fluid):
         can be one among lead, bismuth or lbe
     """
     if fluid == LEAD_KEYWORD:
-        return (176.2*(T - LEAD_MELTING_TEMPERATURE)
-                - 2.4615e-2*(T**2 - LEAD_MELTING_TEMPERATURE**2)
-                + 5.147e-6*(T**3 - LEAD_MELTING_TEMPERATURE**3)
-                + 1.524e6*(T**-1 - LEAD_MELTING_TEMPERATURE**-1))
+        rvalue = (176.2*(T - LEAD_MELTING_TEMPERATURE)
+                  - 2.4615e-2*(T**2 - LEAD_MELTING_TEMPERATURE**2)
+                  + 5.147e-6*(T**3 - LEAD_MELTING_TEMPERATURE**3)
+                  + 1.524e6*(T**-1 - LEAD_MELTING_TEMPERATURE**-1))
     elif fluid == BISMUTH_KEYWORD:
-        return (118.2*(T - BISMUTH_MELTING_TEMPERATURE)
-                + 2.967e-3*(T**2 - BISMUTH_MELTING_TEMPERATURE**2)
-                - 7.183e6*(T**-1 - BISMUTH_MELTING_TEMPERATURE**-1))
+        rvalue = (118.2*(T - BISMUTH_MELTING_TEMPERATURE)
+                  + 2.967e-3*(T**2 - BISMUTH_MELTING_TEMPERATURE**2)
+                  - 7.183e6*(T**-1 - BISMUTH_MELTING_TEMPERATURE**-1))
     elif fluid == LBE_KEYWORD:
-        return (164.8*(T - LBE_MELTING_TEMPERATURE)
-                - 1.97e-2*(T**2 - LBE_MELTING_TEMPERATURE**2)
-                + 4.167e-6*(T**3 - LBE_MELTING_TEMPERATURE**3)
-                + 4.56e5*(T**-1 - LEAD_MELTING_TEMPERATURE**-1))
+        rvalue = (164.8*(T - LBE_MELTING_TEMPERATURE)
+                  - 1.97e-2*(T**2 - LBE_MELTING_TEMPERATURE**2)
+                  + 4.167e-6*(T**3 - LBE_MELTING_TEMPERATURE**3)
+                  + 4.56e5*(T**-1 - LEAD_MELTING_TEMPERATURE**-1))
     else:
         raise ValueError("Fluid can be one among: {:s},"
                          "{:s}, {:s}. {:s} was provided"
                          .format(LEAD_KEYWORD, BISMUTH_KEYWORD,
                                  LBE_KEYWORD, fluid))
+
+    return rvalue
 
 
 def mi(T, fluid):
@@ -245,16 +258,18 @@ def mi(T, fluid):
         can be one among lead, bismuth or lbe
     """
     if fluid == LEAD_KEYWORD:
-        return 4.55e-4*np.exp(1069/T)
+        rvalue = 4.55e-4*np.exp(1069/T)
     elif fluid == BISMUTH_KEYWORD:
-        return 4.456e-4*np.exp(780/T)
+        rvalue = 4.456e-4*np.exp(780/T)
     elif fluid == LBE_KEYWORD:
-        return 4.94e-4*np.exp(754.1/T)
+        rvalue = 4.94e-4*np.exp(754.1/T)
     else:
         raise ValueError("Fluid can be one among: {:s},"
                          "{:s}, {:s}. {:s} was provided"
                          .format(LEAD_KEYWORD, BISMUTH_KEYWORD,
                                  LBE_KEYWORD, fluid))
+
+    return rvalue
 
 
 def r(T, fluid):
@@ -270,16 +285,18 @@ def r(T, fluid):
         can be one among lead, bismuth or lbe
     """
     if fluid == LEAD_KEYWORD:
-        return (67.0 + 0.0471*T)*1e-8
+        rvalue = (67.0 + 0.0471*T)*1e-8
     elif fluid == BISMUTH_KEYWORD:
-        return (98.96 + 0.0554*T)*1e-8
+        rvalue = (98.96 + 0.0554*T)*1e-8
     elif fluid == LBE_KEYWORD:
-        return (90.9 + 0.048*T)*1e-8
+        rvalue = (90.9 + 0.048*T)*1e-8
     else:
         raise ValueError("Fluid can be one among: {:s},"
                          "{:s}, {:s}. {:s} was provided"
                          .format(LEAD_KEYWORD, BISMUTH_KEYWORD,
                                  LBE_KEYWORD, fluid))
+
+    return rvalue
 
 
 def conductivity(T, fluid):
@@ -295,13 +312,15 @@ def conductivity(T, fluid):
         can be one among lead, bismuth or lbe
     """
     if fluid == LEAD_KEYWORD:
-        return 9.2 + 0.011*T
+        rvalue = 9.2 + 0.011*T
     elif fluid == BISMUTH_KEYWORD:
-        return 7.34 + 9.5e-3*T
+        rvalue = 7.34 + 9.5e-3*T
     elif fluid == LBE_KEYWORD:
-        return 3.284 + 1.617e-2*T - 2.305e-6*T**2
+        rvalue = 3.284 + 1.617e-2*T - 2.305e-6*T**2
     else:
         raise ValueError("Fluid can be one among: {:s},"
                          "{:s}, {:s}. {:s} was provided"
                          .format(LEAD_KEYWORD, BISMUTH_KEYWORD,
                                  LBE_KEYWORD, fluid))
+
+    return rvalue
