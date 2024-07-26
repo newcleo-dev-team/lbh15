38d8d4cf4f2d3364ba2f8673b2a9ace7c9260f25
Sami Amestas <sami.amestas@avogadro.newcleo.local>
2024-07-26 15:17:47 +0200

    [185_bismuth_contamination], issue #185, Modifications made based on the suggestions made by lelaus and panDanieleN in pull request #192

--- lbh15/properties/bismuth_thermochemical_properties/bismuth_contamination.py
+++ lbh15/properties/bismuth_thermochemical_properties/bismuth_contamination.py
@@ -2,20 +2,23 @@
 for contamination assessment"""
 from typing import List
 from scipy.constants import atm
+import numpy as np
 from ..interface import PropertyInterface
 from ..._decorators import range_warning
+from ..._commons import BISMUTH_MELTING_TEMPERATURE as T_m0
+from ..._commons import BISMUTH_BOILING_TEMPERATURE as T_b0
 
 
-class BismuthPoloniumActivityCoefficientInterfaceJoy1963(PropertyInterface):
+class BismuthPoloniumActivityCoefficientJoy1963(PropertyInterface):
     """
-    Liquid lead *Polonium compounds activity coeffcient* property class
+    Liquid lead *Polonium compound activity coeffcient* property class
     implementing the correlation by *joy1963*.
     """
     @range_warning
     def correlation(self, T: float, p: float = atm,
                     verbose: bool = False) -> float:
         """
-        Returns the value of the *Polonium compounds activity coefficient* by
+        Returns the value of the *Polonium compound activity coefficient* by
         applying the property correlation.
 
         Parameters
@@ -32,16 +35,16 @@ class BismuthPoloniumActivityCoefficientInterfaceJoy1963(PropertyInterface):
         Returns
         -------
         float:
-            activity coefficient in :math:`[]`
+            activity coefficient in :math:`[-]`
         """
-        return 10**((- 2272.7 / T) + 0.1316)
+        return np.power(10,(- 2728.3 / T) + 1.1176)
 
     @property
     def name(self) -> str:
         """
         str : Name of the property
         """
-        return "gamma_BiPo"
+        return "gamma_BiPo_a"
 
     @property
     def correlation_name(self) -> str:
@@ -55,14 +58,14 @@ class BismuthPoloniumActivityCoefficientInterfaceJoy1963(PropertyInterface):
         """
         str : Activity coefficient unit
         """
-        return "[]"
+        return "[-]"
 
     @property
     def long_name(self) -> str:
         """
         str : Polonium activity coefficient long name
         """
-        return "Activity coefficient of Polonium in pure bismuth"
+        return "Activity coefficient of Polonium"
 
     @property
     def description(self) -> str:
@@ -80,16 +83,83 @@ class BismuthPoloniumActivityCoefficientInterfaceJoy1963(PropertyInterface):
         return [723.0, 1123.0]
 
 
-class BismuthIodineVapourPressureInterfaceCubicciotti1959(PropertyInterface):
+class BismuthPoloniumActivityCoefficient(PropertyInterface):
     """
-    Liquid bismuth *Iodine compounds vapour pressure* property class
+    Liquid lead *Polonium compound activity coeffcient* property class
+    implementing the correlation by *lbh15*.
+    """
+    @range_warning
+    def correlation(self, T: float, p: float = atm,
+                    verbose: bool = False) -> float:
+        """
+        Returns the value of the *Polonium compound activity coefficient* by
+        applying the property correlation.
+
+        Parameters
+        ----------
+        T : float
+            Temperature in :math:`[K]`
+        p : float, optional
+            Pressure in :math:`[Pa]`, by default the atmospheric pressure
+            value, i.e., :math:`101325.0 Pa`
+        verbose : bool, optional
+            `True` to tell the decorator to print a warning message in case of
+            range check failing, `False` otherwise. By default, `False`
+
+        Returns
+        -------
+        float:
+            activity coefficient in :math:`[-]`
+        """
+        return np.power(10,(- 2272.7 / T) + 0.1316)
+
+    @property
+    def name(self) -> str:
+        """
+        str : Name of the property
+        """
+        return "gamma_BiPo_b"
+
+    @property
+    def units(self) -> str:
+        """
+        str : Activity coefficient unit
+        """
+        return "[-]"
+
+    @property
+    def long_name(self) -> str:
+        """
+        str : Polonium activity coefficient long name
+        """
+        return "Activity coefficient of Polonium"
+
+    @property
+    def description(self) -> str:
+        """
+        str : Polonium activity coefficient description
+        """
+        return f"{self.long_name} in liquid bismuth"
+
+    @property
+    def range(self) -> List[float]:
+        """
+        List[float] : Temperature validity range of the activity coefficient
+        correlation function
+        """
+        return [923.0, 1038.0]
+
+
+class BismuthIodineVapourPressureCubicciotti1959(PropertyInterface):
+    """
+    Liquid bismuth *BiI3 Iodide compound vapour pressure* property class
     implementing the correlation by *cubicciotti1959*.
     """
     @range_warning
     def correlation(self, T: float, p: float = atm,
                     verbose: bool = False) -> float:
         """
-        Returns the value of the *Iodine compounds vapour pressure* by
+        Returns the value of the *BiI3 Iodide compound vapour pressure* by
         applying the property correlation.
 
         Parameters
@@ -108,7 +178,7 @@ class BismuthIodineVapourPressureInterfaceCubicciotti1959(PropertyInterface):
         float:
             pressure in :math:`[Pa]`
         """
-        return 10**((- 4310 / T) + 10.29)
+        return np.power(10,(- 4310 / T) + 10.29)
 
     @property
     def name(self) -> str:
@@ -134,21 +204,21 @@ class BismuthIodineVapourPressureInterfaceCubicciotti1959(PropertyInterface):
     @property
     def long_name(self) -> str:
         """
-        str : Iodine vapour pressure long name
+        str : BiI3 Iodide vapour pressure long name
         """
-        return "Vapour pressure of Iodine in pure bismuth"
+        return "Vapour pressure of BiI3 Iodide"
 
     @property
     def description(self) -> str:
         """
-        str : Iodine vapour pressure description
+        str : BiI3 Iodide vapour pressure description
         """
         return f"{self.long_name} in liquid bismuth"
 
     @property
     def range(self) -> List[float]:
         """
-        List[float] : Temperature validity range of the Iodine vapour pressure
-        correlation function
+        List[float] : Temperature validity range of the BiI3 Iodide
+        vapour pressure correlation function
         """
-        return [544.6, 1831.0]
+        return [T_m0, T_b0]
